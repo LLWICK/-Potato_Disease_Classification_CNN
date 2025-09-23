11c4a30ae080ff25b1baf08e4db535183dfbb479
LLWICK <linwick679@gmail.com>
2025-09-24 05:12:44 +0530

    fatapi update

--- api/main.py
+++ api/main.py
@@ -1,15 +1,36 @@
 from fastapi import FastAPI, UploadFile
 import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
 
 app = FastAPI()
+
+model = tf.keras.models.load_model("../models/1.h5")
+
+labels = ['Potato___Early_blight', 'Potato___Late_blight', 'Potato___healthy']
+
 
 @app.get("/ping")
 def read_root():
     return {"Hello": "World"}
 
-@app.post("/predict")
+def read_file_as_image(data)-> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    return image
+
+
+@app.post("/predict/")
 async def predict(file: UploadFile):
-    return {"filename": file.filename}
+  
+
+    image = read_file_as_image(await file.read())
+    image_batch = np.expand_dims(image,0)
+    prediction = model.predict(image_batch)
+    ind = np.argmax(prediction[0])
+    accuracy = round(100*np.max(prediction[0]))
+    return {"Prediction": labels[ind], "Accuracy": accuracy}
 
 
 if __name__ == "main":
